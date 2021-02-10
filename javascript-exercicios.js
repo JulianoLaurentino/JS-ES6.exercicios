e88165848dd40d847c59d71ab1a5d5c16d7bfbf5
Juliano Laurentino <66442729+JulianoLaurentino@users.noreply.github.com>
2021-02-10 09:15:05 -0300

    Update javascript-exercicios.js

--- javascript-exercicios.js
+++ javascript-exercicios.js
@@ -95,4 +95,38 @@ console.log(numberspar);
 	       
 const animals = ["lion", "tiger", "elephant", "shark"];
 const cats = animals.filter((animal) animal = "lion" || animal = "tiger");
-console.log(cats);	       
+console.log(cats);	
+	       
+	       
+	       
+/*Crie um programa que leia 6 valores. 
+Você poderá receber valores negativos e/ou positivos como entrada, 
+devendo desconsiderar os valores nulos. Em seguida, 
+apresente a quantidade de valores positivos digitados.*/
+
+let total = 0;
+
+let i = 0;
+
+let valor;
+
+while (i<6)
+
+{
+
+ valor = parseFloat(gets());
+
+ if (valor > 0)
+
+ {
+
+   total = total + 1;
+
+ }
+
+ i = i + 1;
+
+}
+
+console.log(total + " valores positivos");	       
+	       
